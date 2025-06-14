1e5ac1453077118722bb6b083cbed4748da63d07
Aniruddha Sarkar <145282516+Ani811625@users.noreply.github.com>
2025-06-14 14:00:03 +0530

    Update InsertOrderDetails.sql

--- AdventureWorks2022/Stored Procedures/InsertOrderDetails.sql
+++ AdventureWorks2022/Stored Procedures/InsertOrderDetails.sql
@@ -12,7 +12,7 @@ BEGIN
     DECLARE @ReorderLevel INT;
     DECLARE @ActualUnitPrice MONEY;
 
-    -- Get stock and reorder level
+    
     SELECT 
         @Stock = UnitsInStock, 
         @ReorderLevel = ReorderLevel,
@@ -20,33 +20,33 @@ BEGIN
     FROM Products
     WHERE ProductID = @ProductID;
 
-    -- If UnitPrice not given, use Product's UnitPrice
+   
     SET @UnitPrice = ISNULL(@UnitPrice, @ActualUnitPrice);
 
-    -- Check stock availability
+    
     IF @Quantity > @Stock
     BEGIN
         PRINT 'Insufficient stock. Order aborted.';
         RETURN;
     END
 
-    -- Insert into OrderDetails
+    
     INSERT INTO [Order Details] (OrderID, ProductID, UnitPrice, Quantity, Discount)
     VALUES (@OrderID, @ProductID, @UnitPrice, @Quantity, @Discount);
 
-    -- Check if insert was successful
+   
     IF @@ROWCOUNT = 0
     BEGIN
         PRINT 'Failed to place the order. Please try again.';
         RETURN;
     END
 
-    -- Update stock
+    
     UPDATE Products
     SET UnitsInStock = UnitsInStock - @Quantity
     WHERE ProductID = @ProductID;
 
-    -- Check Reorder Level
+   
     IF EXISTS (
         SELECT 1 FROM Products
         WHERE ProductID = @ProductID AND UnitsInStock < ReorderLevel
